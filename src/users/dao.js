debc66fe78574533e4f6250d21290ae3fbe53d40
Andrew <andrew0005li@gmail.com>
2023-12-06 14:32:05 -0500

    added update coins

--- src/users/dao.js
+++ src/users/dao.js
@@ -21,11 +21,11 @@ const model = mongoose.model("users", userSchema);
 export const createUser = (user) => model.create({...user, role: "USER", coins: 500});
 export const createAdmin = (user) => model.create({ ...user, role: "ADMIN", coins: 9999999});
 export const findAllUsers = () => model.find();
-export const findUserById = (userId) => model.findById(userId);
 export const findUserByUsername = (username) =>
   model.findOne({ username: username });
 export const findUserByCredentials = (username, password) =>
   model.findOne({ username, password });
-export const updateUser = (userId, user) =>
-  model.updateOne({ _id: userId }, { $set: user });
-export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+export const updateUserCoins = (username, coins) =>
+  model.updateOne({ username: username }, { $set: { coins } });
+
+
--- src/users/routes.js
+++ src/users/routes.js
@@ -1,51 +1,60 @@
 import * as dao from "./dao.js";
 
 export default function UserRoutes(app) {
-  const signIn = async (req, res) => {
-    const { username, password } = req.body;
-    const currentUser = await dao.findUserByCredentials(username, password);
-    if (!currentUser) {
-      res.status(401).json({ message: "Incorrect username or password." });
-      return;
-    }
-    req.session["currentUser"] = currentUser;
-    res.json(currentUser);
-  };
+      const updateUserCoins = async (req, res) => {
+        const { username } = req.params;
+        const { coins } = req.body;
+        const status = await dao.updateUserCoins(username, coins);
+        res.json(status);
+      };
 
-  const signOut = (req, res) => {
-    req.session.destroy();
-    res.sendStatus(200);
-  };
+      const signIn = async (req, res) => {
+        const { username, password } = req.body;
+        const currentUser = await dao.findUserByCredentials(username, password);
+        if (!currentUser) {
+          res.status(401).json({ message: "Incorrect username or password." });
+          return;
+        }
+        req.session["currentUser"] = currentUser;
+        res.json(currentUser);
+      };
 
-  const signUpAsUser = async (req, res) => {
-    const existingUser = await dao.findUserByUsername(req.body.username);
-    if (existingUser) {
-      res.status(400).json({ message: "Username already exists." });
-      return;
-    }
-    const newUser = await dao.createUser(req.body);
-    req.session["currentUser"] = newUser;
-    res.json(newUser);
-  };
+      const signOut = (req, res) => {
+        req.session.destroy();
+        res.sendStatus(200);
+      };
 
-  const signUpAsAdmin = async (req, res) => {
-    const existingUser = await dao.findUserByUsername(req.body.username);
-    if (existingUser) {
-      res.status(400).json({ message: "Username already exists." });
-      return;
-    }
+      const signUpAsUser = async (req, res) => {
+        const existingUser = await dao.findUserByUsername(req.body.username);
+        if (existingUser) {
+          res.status(400).json({ message: "Username already exists." });
+          return;
+        }
+        const newUser = await dao.createUser(req.body);
+        req.session["currentUser"] = newUser;
+        res.json(newUser);
+      };
 
-    const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
-    if (req.body.admin_password !== ADMIN_PASSWORD) {
-      res.status(401).json({ message: "Incorrect admin password." });
-      return;
-    }
+      const signUpAsAdmin = async (req, res) => {
+        const existingUser = await dao.findUserByUsername(req.body.username);
+        if (existingUser) {
+          res.status(400).json({ message: "Username already exists." });
+          return;
+        }
 
-    const newUser = await dao.createAdmin(req.body);
-    req.session["currentUser"] = newUser;
-    res.json(newUser);
-  };
+        const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
+        if (req.body.admin_password !== ADMIN_PASSWORD) {
+          res.status(401).json({ message: "Incorrect admin password." });
+          return;
+        }
 
+        const newUser = await dao.createAdmin(req.body);
+        req.session["currentUser"] = newUser;
+        res.json(newUser);
+      };
+
+
+  app.put("/api/users/:username/coins", updateUserCoins);
   app.post("/api/users/signin", signIn);
   app.post("/api/users/signout", signOut);
   app.post("/api/users/signup-user", signUpAsUser);
